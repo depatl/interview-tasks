34f7228372a3d7da7d51312c3b10a32a19d641df
Denis Patlazhan <dpatlazh@cisco.com>
2025-02-11 12:30:02 -0800

    Update collect_metrics.py

--- exercise1/collect_metrics.py
+++ exercise1/collect_metrics.py
@@ -5,6 +5,8 @@ collect_metrics.py
 Script to read a list of server IPs from servers.txt, simulate an SSH connection,
 and generate a random CPU usage metric for each server.
 
+Output in JSON format: [{"server": IP, "cpu": UTILIZATION }]
+
 Usage:
     python collect_metrics.py
 """
